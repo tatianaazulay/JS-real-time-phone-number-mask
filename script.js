052975868e38c70f37c1300357697934ccab26b8
Your Name <you@example.com>
2020-05-01 23:15:59 -0400

    4TODO Set the value of the textbox with the formatted value

--- script.js
+++ script.js
@@ -14,7 +14,29 @@ function inputChanged(e){
     /* TODO:  Use replace function to ignore extra - character and all other non-digit characters in the imput*/
     phoneNum = phoneNum.replace(new RegExp(/[^\d]/g), ''); // Remove every non-digit character 
     console.log("Key Pressed (real phone number)= "+phoneNum);//displays UNFORMATTED phoneNum, which ignores all non-digit characters
-};  /* end TODO */
+    /* end TODO */
+    // TODO: Set the value of the textbox with the formatted value
+    if(phoneNum.match(/^[0-9]*$/g)) {
+      phoneNum = formatPhoneNumber(phoneNum);
+      console.log("Formatted = "+phoneNum); //displays FORMATTED phoneNum
+    if(phoneNum !== "") {
+      displayedNum.value = phoneNum;
+    } else {
+      displayedNum.value = "";
+    }
+    }
+    /* end TODO */
+  };
+  function formatPhoneNumber(value) {
+    if(value.length > 3 && value.length <= 6 ){
+      value = value.slice(0,3) + "-" + value.slice(3);
+      newCursor=cursor++;
+    } else if(value.length > 6 ){
+      value =value.slice(0,3) + "-" + value.slice(3,6)+ "-" + value.slice(6);
+      newCursor=cursor+2;
+      }
+    return value;
+  };
 
 
-   
+ 
